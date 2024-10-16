57d9f5d7cd92f96f0cda8a6404f96b7f86dec888
hemant jatal <hemantjatal59@gmail.com>
2024-10-17 00:07:52 +0530

    Refactor code to remove unused struct and function

--- src/iterators.rs
+++ src/iterators.rs
@@ -7,3 +7,65 @@ fn main(){
     }
     }
     
+
+
+    fn main(){
+        let mut nums = vec![1,2,3,4,5];
+        let v1_iter = nums.iter();
+    
+        for val in v1_iter {
+            println!("got {val}")
+        }
+    
+        let v1_iter_mut = nums.iter_mut();
+    
+        for val in v1_iter_mut {
+            *val = *val + 1;        
+        }
+    
+        println!("{:?}" ,nums);
+    }
+
+
+fn main(){
+    let nums = vec![1,2,3];
+    let mut iter = nums.iter();
+ 
+    while let Some(val) = iter.next() {
+     print!("{}",val);
+    }
+ }
+ 
+ fn main(){
+    let mut nums = vec![1,2,3];
+    let mut iter = nums.iter_mut();
+ 
+    let first_number = iter.next();
+    let second_number = iter.next();
+    let third_number = iter.next();
+ 
+    while let Some(val) = iter.next() {
+     print!("{}",val);
+    }  
+ }
+
+
+
+fn main(){
+    let nums = vec![1,2,3,4,5,6,7,8,9];
+ 
+    let iter = nums.iter();
+ 
+    let  filered_iter = iter.filter(|x| *x%2==0).map(|x| x*2);
+ 
+ 
+ 
+  let new_vec:Vec<i32> = filered_iter.collect();
+ 
+  print!("{:?}",new_vec);
+   
+ 
+ }
+ 
+ 
+    
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
 
+
+
 fn main(){
+   let nums = vec![1,2,3,4,5,6,7,8,9];
+
+   let iter = nums.iter();
+
+   let  filered_iter = iter.filter(|x| *x%2==0).map(|x| x*2);
+
+
+
+ let new_vec:Vec<i32> = filered_iter.collect();
+
+ print!("{:?}",new_vec);
+  
 
 }
